ebba3dbe1d39e075780f4e1b01271f25f5d5f013
nstanovic <60545818+nstanovic@users.noreply.github.com>
2022-05-12 16:51:22 -0400

    Modify wording of Disk Cleanup function

--- Cleaning.ps1
+++ Cleaning.ps1
@@ -81,7 +81,7 @@ function DisplayDiskSpaceBeforeCleaning ()
 function RunDiskCleanup ()
 {
     cleanmgr /verylowdisk
-    Write-Host "Running Windows Disk Cleanup with all..."  -ForegroundColor Yellow
+    Write-Host "Running Windows Disk Cleanup..."  -ForegroundColor Yellow
     do 
         {
             
